6ec29a0ef37d5fd20c64af3507d9dac4f72bd79b
Joao S. O. Bueno <gwidion@gmail.com>
2017-02-21 20:39:46 -0300

    First metaclass: naming descriptor fields

--- persist.py
+++ persist.py
@@ -10,8 +10,31 @@ class Field:
         del instance.__dict__[self.name]
 
 
+class StrField(Field):
+    def __set__(self, instance, value):
+        if not isinstance(value, str):
+            raise TypeError(f"{self.name} field can only contain strings")
+        super().__set__(instance, value)
 
-class Pessoa:
+
+class IntField(Field):
+    def __set__(self, instance, value):
+        try:
+            value = int(value)
+        except ValueError:
+            raise TypeError(f"{self.name} field value must be convertible to int")
+        super().__set__(instance, value)
+
+
+class ModelBase(type):
+    def __new__(metacls, name, bases, namespace):
+        for attr_name, value in namespace.items():
+            if isinstance(value, Field):
+                value.name = attr_name
+        return super().__new__(metacls, name, bases, namespace)
+
+
+class Pessoa(metaclass=ModelBase):
 
     nome = StrField()
     idade = IntField()

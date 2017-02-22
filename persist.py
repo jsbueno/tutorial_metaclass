044776cd44ad016375c64dd5accd98c30a595cb5
Joao S. O. Bueno <gwidion@gmail.com>
2017-02-22 18:32:35 -0300

    Add attribute-setting restriction UUID based ID fields, Model class

--- persist.py
+++ persist.py
@@ -1,3 +1,4 @@
+import uuid
 
 class Field:
     def __set__(self, instance, value):
@@ -6,8 +7,12 @@ class Field:
         if instance:
             return instance.__dict__[self.name]
         return self
+
     def __del__(self, instance):
         del instance.__dict__[self.name]
+
+    def serialize(self, instance):
+        return self.__get__(instance, None)
 
 
 class StrField(Field):
@@ -25,16 +30,46 @@ class IntField(Field):
             raise TypeError(f"{self.name} field value must be convertible to int")
         super().__set__(instance, value)
 
+class UUIDField(Field):
+    def __set__(self, instance, value):
+        if isinstance(value, str):
+            value = uuid.UUID(str)
+        super().__set__(instance, value)
+
+    def serialize(self, instance):
+        value = super().serialize(instance)
+        return str(value)
+
+
+def model_setattr(self, attr, value):
+    """ Filter __setattribute__ method to avoid that
+    any attribute that is not defined as a persistent
+    property is ever set.
+    """
+    if not hasattr(self.__class__, attr):
+       raise TypeError(f"{attr} is not a valid attribute for {self.__class__} objects")
+    object.__setattr__(self, attr, value)
+
 
 class ModelBase(type):
     def __new__(metacls, name, bases, namespace):
         for attr_name, value in namespace.items():
             if isinstance(value, Field):
                 value.name = attr_name
+        if not "__setattr__" in namespace:
+            namespace["__setattr__"] = model_setattr
+
         return super().__new__(metacls, name, bases, namespace)
 
 
-class Pessoa(metaclass=ModelBase):
+class Model(metaclass=ModelBase):
+    id = UUIDField()
+
+    def __init__(self):
+        self.id = uuid.uuid4()
+
+
+class Pessoa(Model):
 
     nome = StrField()
     idade = IntField()
